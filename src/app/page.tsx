ee68db007d8408d1d0d533892b87376feab7e33a
Zara <122231647+zarail@users.noreply.github.com>
2025-05-01 17:35:59 +0200

    Modify classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-teal-900 text-stone-50">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[16px] row-start-2 sm:items-start tracking-wider">
         <header className="fixed top-0 right-0 left-0 z-50 w-full px-6 sm:px-10">
           <div className="flex justify-end items-center">

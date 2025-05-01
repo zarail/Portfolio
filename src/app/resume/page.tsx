4cb5cc265a6ec1d3cc331d8f25bfd2665607f465
Zara <122231647+zarail@users.noreply.github.com>
2025-05-01 18:52:03 +0200

    Add resume contents

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,8 +1,133 @@
 export default function ResumePage() {
   return (
-    <div>
-      <h1>Resume Page</h1>
-      <p>Welcome to the resume page!</p>
-    </div>
+    <main className="p-10">
+      <div>
+        <p>Zahra Ilkhan</p>
+        <p>Cologne, Germany</p>
+        <p>+4900000000000</p>
+        <p>zahrailkhan@gmail.com</p>
+        <p>[LinkedIn]</p>
+        <p>[GitHub]</p>
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Education</h2>
+
+        <div>
+          <h3>Software Quality Assurance Traineeship</h3>
+          <p>Jan. 2025 – Feb. 2025</p>
+          <ul>
+            <li>ISTQB® CTFL Certification</li>
+            <li>
+              Interface testing with SoapUI, web apps with Selenium, Ranorex for
+              desktop
+            </li>
+            <li>Test management tools: Jira, aqua Web</li>
+            <li>Agile methodologies</li>
+          </ul>
+        </div>
+
+        <br />
+
+        <div>
+          <h3>Fullstack Web Development with Next.js and TypeScript</h3>
+          <p>Aug. 2024 – Sept. 2024</p>
+          <ul>
+            <li>TypeScript for secure JS development</li>
+            <li>Websites and SSR with Next.js</li>
+            <li>Using GraphQL as an alternative to REST APIs</li>
+          </ul>
+        </div>
+
+        <br />
+
+        <div>
+          <h3>Programming with JavaScript Framework React</h3>
+          <p>Jul. 2024 – Aug. 2024</p>
+          <ul>
+            <li>UI and dynamic website development with React</li>
+            <li>REST APIs and SPAs with React Router</li>
+            <li>Version control and deployment with GIT</li>
+          </ul>
+        </div>
+
+        <br />
+
+        <div>
+          <h3>Fullstack Web Development Bootcamp</h3>
+          <p>Jan. 2023 – Mar. 2023</p>
+          <ul>
+            <li>Ruby programming and web development with Ruby on Rails</li>
+            <li>Frontend with HTML, CSS, JavaScript</li>
+            <li>API development and SQL database management</li>
+          </ul>
+        </div>
+
+        <br />
+
+        <div>
+          <h3>Bachelor of Arts – Computer Science and English</h3>
+          <p>2018 – 2023</p>
+          <ul>
+            <li>
+              Internship: Digital Teaching & Learning (Mar. 2021 – May 2021)
+            </li>
+            <li>Study Abroad: Summer 2022</li>
+          </ul>
+        </div>
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Work Experience</h2>
+        <h3>Web Development Teaching Assistant</h3>
+        <p>Apr. 2023 – Jun. 2023</p>
+        <p>Supported students in learning and tasks</p>
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Skills</h2>
+        <p>
+          JavaScript | TypeScript | React.js | Next.js | HTML5 | CSS3 | Tailwind
+          CSS | Bootstrap | SASS | Java | Ruby on Rails | Ruby | Active Record |
+          C | SQL | REST APIs | GraphQL | Git | npm | ESLint | SoapUI | Selenium
+          | Ranorex | ISTQB® CTFL | Agile methodologies | Jira | UX/UI | Figma |
+          Prototyping | Adobe Photoshop
+        </p>
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Projects</h2>
+        <div>
+          <h3>Hey Local</h3>
+          <p>Capstone Project – Next.js/TypeScript</p>
+        </div>
+        <div>
+          <h3>The Capital City Game</h3>
+          <p>Capstone Project – React</p>
+        </div>
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Languages</h2>
+        <p>German: fluent | English: fluent | Persian: native</p>
+      </div>
+
+      <br />
+
+      <div>
+        <h2>Interests</h2>
+        <p>Cycling | Playing the Drums</p>
+      </div>
+    </main>
   );
 }

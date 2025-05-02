93dddb405696410f54ce1f7c5ac1cb62428ba278
Zara <122231647+zarail@users.noreply.github.com>
2025-05-02 13:51:00 +0200

    Move navbar contents to its own

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,9 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[16px] row-start-2 sm:items-start tracking-wider">
-        <header className="fixed top-0 right-0 left-0 z-50 w-full px-6 sm:px-10">
-          <div className="flex justify-end items-center">
-            <nav className="flex gap-6 flex-wrap items-center justify-end mt-12 pr-4">
-              <Link
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  aria-hidden
-                  src="/home.svg"
-                  alt="File icon"
-                  width={16}
-                  height={16}
-                />
-                Home
-              </Link>
-              <Link
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="/resume"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  aria-hidden
-                  src="/resume.svg"
-                  alt="File icon"
-                  width={16}
-                  height={16}
-                />
-                Resume
-              </Link>
-              <a
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://vercel.com/templates"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  aria-hidden
-                  src="/projects.svg"
-                  alt="Window icon"
-                  width={16}
-                  height={16}
-                />
-                Projects
-              </a>
-              <a
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://nextjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  aria-hidden
-                  src="/contact.svg"
-                  alt="Globe icon"
-                  width={16}
-                  height={16}
-                />
-                Contact
-              </a>
-            </nav>
-          </div>
-        </header>
         <Image
           className="mb-2 mx-auto"
           src="\logo.svg"

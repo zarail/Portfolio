0c2d005480797bc88a1c0cef8c8ce4c7120b97ac
Zara <122231647+zarail@users.noreply.github.com>
2025-05-20 17:12:44 +0200

    Remove Navbar component from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Navbar from './components/Navbar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -39,7 +38,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pt-16`}
       >
-        <Navbar />
         {children}
       </body>
     </html>

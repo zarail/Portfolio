5d2e53823f55a1031ae23a5d0c6c0a6dcbc490e7
Zara <122231647+zarail@users.noreply.github.com>
2025-05-23 09:53:55 +0200

    Rename ResumePage to AboutPage for consistency with routing

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -8,7 +8,7 @@ type Props = {
   };
 };
 
-export default function ResumePage({ params }: Props) {
+export default function AboutPage({ params }: Props) {
   const dict: any = getDictionary(params.locale) || {};
   const about = dict.about || {};
 
